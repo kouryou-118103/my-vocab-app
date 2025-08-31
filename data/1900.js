6e9da3d547e1b4ff26ba315be60f3e71a4627c48
kouryou-118103 <94727510+kouryou-118103@users.noreply.github.com>
2025-08-31 13:38:38 +0900

    Update 1900.js

--- data/1900.js
+++ data/1900.js
@@ -392,6 +392,7 @@ if (selected === 正解) {
   result += `<p class="question">${escapeHTML(表示語句)}<br>${escapeHTML(正解)}</p>`;
   result += `<p>${escapeHTML(userLang)}<br>${escapeHTML(selected).replace(/(.{100})/g, '$1<br>')}</p>`;  ミス記録.push({ 表示語句, 正解, あなたの答え: selected });
 }
+console.log("保存直前", wordStats);
 localStorage.setItem("wordStats", JSON.stringify(wordStats));
 result += `<p>${問題数}問中${正解回数}問正解 (${Math.floor(正解回数 / 問題数 * 100)}%)</p>`;
 
